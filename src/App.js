f94b6c7969e3fbeca3a6e3280212af4a7281a5eb
hajczek <i.hajczewska@gmail.com>
2020-01-22 09:05:12 +0100

    Added span elements to session and break elements with default value

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ function App() {
       <div id="break-label">
         <p>Break length</p>
         <button id="break-decrement">-</button>
+        <span id="break-length">5</span>
         <button id="break-increment">+</button>
       </div>
       <div id="session-label">
         <p>Session length</p>
         <button id="session-decrement">-</button>
+        <span id="session-length">25</span>
         <button id="session-increment">+</button>
       </div>
     </div>
