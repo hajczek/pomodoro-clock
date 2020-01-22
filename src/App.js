35097296fd459c4bc9c3ff0ad8f59f2edbb5a2b6
hajczek <i.hajczewska@gmail.com>
2020-01-22 09:03:08 +0100

    Added buttons for decrement to break and session elements

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ function App() {
       <div id="break-label">
         <p>Break length</p>
         <button id="break-decrement">-</button>
+        <button id="break-increment">+</button>
       </div>
       <div id="session-label">
         <p>Session length</p>
         <button id="session-decrement">-</button>
+        <button id="session-increment">+</button>
       </div>
     </div>
   );
