e4b2566f2394923d8d38829892f7aef0f031be12
hajczek <i.hajczewska@gmail.com>
2020-01-22 09:01:34 +0100

    Added decrement buttons for break and session length elements

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
       <h1>Pomodoro Clock</h1>
       <div id="break-label">
         <p>Break length</p>
+        <button id="break-decrement">-</button>
       </div>
       <div id="session-label">
         <p>Session length</p>
+        <button id="session-decrement">-</button>
       </div>
     </div>
   );
