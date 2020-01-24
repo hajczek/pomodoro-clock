0063805be06b8ee9c267a903cdea45ac399e6a7f
hajczek <i.hajczewska@gmail.com>
2020-01-24 08:26:25 +0100

    Changed App component to functional

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Pomodoro Clock</h1>
-      <div id="break-label">
-        <p>Break length</p>
-        <button id="break-decrement">-</button>
-        <span id="break-length">5</span>
-        <button id="break-increment">+</button>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Pomodoro Clock</h1>
+        <div id="break-label">
+          <p>Break length</p>
+          <button id="break-decrement">-</button>
+          <span id="break-length">5</span>
+          <button id="break-increment">+</button>
+        </div>
+        <div id="session-label">
+          <p>Session length</p>
+          <button id="session-decrement">-</button>
+          <span id="session-length">25</span>
+          <button id="session-increment">+</button>
+        </div>
+        <div id="timer-label">
+          <h3>Session</h3>
+          <p id="time-left">mm:ss</p>
+        </div>
+        <div id="timer-control">
+          <button id="start_stop">Start/Stop</button>
+          <button id="reset">Reset</button>
+        </div>
       </div>
-      <div id="session-label">
-        <p>Session length</p>
-        <button id="session-decrement">-</button>
-        <span id="session-length">25</span>
-        <button id="session-increment">+</button>
-      </div>
-      <div id="timer-label">
-        <h3>Session</h3>
-        <p id="time-left">mm:ss</p>
-      </div>
-      <div id="timer-control">
-        <button id="start_stop">Start/Stop</button>
-        <button id="reset">Reset</button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
