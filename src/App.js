cef96bcaea529d0dbe8b7445d86241a6dfe963e1
hajczek <i.hajczewska@gmail.com>
2020-01-28 19:29:17 +0100

    Fixed class to className, delete setSession function and add sessionDecrement function

--- src/App.js
+++ src/App.js
@@ -5,54 +5,55 @@ class App extends Component {
     super(props);
     this.state = {
       breakLength: 5,
-      session: 25,
-      sessionSet: 25
+      session: 25
     };
   }
 
-  setSession = () => {
-    const d = new Date();
-    d.setMinutes(d.getMinutes() + this.state.sessionSet);
-    this.setState = {
-      sessionSet: d.getTime()
-    };
+  sessionDecrement = () => {
+    this.setState({
+      session: this.state.session - 1
+    });
   };
 
   render() {
     return (
       <div className="App">
         <h1>Pomodoro Clock</h1>
-        <div class="box" id="break-label">
+        <div className="box" id="break-label">
           <p>Break length</p>
-          <button class="btn-small" id="break-decrement">
+          <button className="btn-small" id="break-decrement">
             -
           </button>
           <span id="break-length">{this.state.breakLength}</span>
-          <button class="btn-small" id="break-increment">
+          <button className="btn-small" id="break-increment">
             +
           </button>
         </div>
-        <div class="box" id="session-label">
+        <div className="box" id="session-label">
           <p>Session length</p>
-          <button class="btn-small" id="session-decrement">
+          <button
+            className="btn-small"
+            id="session-decrement"
+            onClick={this.sessionDecrement}
+          >
             -
           </button>
           <span id="session-length">{this.state.session}</span>
-          <button class="btn-small" id="session-increment">
+          <button className="btn-small" id="session-increment">
             +
           </button>
         </div>
-        <div class="box" id="timer-label">
+        <div className="box" id="timer-label">
           <h3>Session</h3>
-          <p class="time" id="time-left">
-            {this.state.sessionSet}
+          <p className="time" id="time-left">
+            {this.state.session}
           </p>
         </div>
-        <div class="box" id="timer-control">
-          <button class="btn-big" id="start_stop">
+        <div className="box" id="timer-control">
+          <button className="btn-big" id="start_stop">
             Start/Stop
           </button>
-          <button class="btn-big" id="reset">
+          <button className="btn-big" id="reset">
             Reset
           </button>
         </div>
